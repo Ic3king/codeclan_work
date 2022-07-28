33573b5482c1511ac1fc48f2286a4586573e56ec
Iveri Cikaitoga <()vutejunior@gmail.com>
2022-07-28 15:57:44 +0100

    hello this is my labs

---
+++ comp_app/src/App.js
@@ -0,0 +1,45 @@
+
+import './App.css';
+import MyImage from "./"
+
+function App() {
+  return (
+  <>
+  <div className="search-bar">
+  <input />
+  </div>
+    <div className="image-pic">
+    <img draggable="false" alt="rick astley" width="500" src="https://i.ytimg.com/vi/dQw4w9WgXcQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLDRxusbm2_TGTnDWEIhBTYW2cUQkw/path"/>
+    </div>
+    <p className="video-title">
+      Rick Astely - Never Gonna Give You Up (Official Music Video)
+    </p>
+    <p className="video-stats">
+      1,256,456,153 views &#183; 25 Oct 2009
+    </p>
+    <div>
+    <p className="video-author">
+    <img alt="Rick Astley" height="136" width="136" src=" id="im draggable="false" class="style-scope yt-img-shadow" alt="" height="136" width="136" src="face_RK.jpeg"/>
+    Rick Astely &#10003;
+    </p>
+    </div>
+    <p className="video-description">
+      The official video for “Never Gonna Give You Up” by Rick Astley
+    </p>
+    <button className="subscription_button"> SUBSCRIBE </button>
+    <button className="join_button"> Join</button>
+    <button className="tweet_button">Tweet</button>
+    <a href = "https://youtube.com" target = "_blank" > Link to Youtube</a>
+    <p className="apple-text">
+      Shop Early for the best selection of holiday favourites &#62; >
+    </p>
+    <p className="apple-info">
+      Shop Early for the <strong>best selection</strong> of <u>holiday</u> favourites. <span className="apple-shop">
+        Shop Now
+      </span>
+    </p>
+  </>
+  )
+}
+
+export default App;
